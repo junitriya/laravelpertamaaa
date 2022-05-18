98530f372ffc54419bb48c5c5845daed565c95f8
junitriya <junitriya32@gmail.com>
2022-05-18 09:14:03 +0100

    comit7

--- resources/views/Groups/edit.blade.php
+++ resources/views/Groups/edit.blade.php
@@ -9,3 +9,18 @@
     @method('PUT')
     <div class="mb-3">
         <label for="name" class="form-label">Name</label>
+        <input type="text" class="form-control" id="name" name="name" value="{{ old('name') ? old('name') : $group['name'] }}">
+        @error('name')
+        <div class="alert alert-danger">{{ $message }}</div>
+        @enerror
+</div>
+<div class="mb-3">
+    <label for="description" class="form-label">Description</label>
+    <input type="text" class="form-control" id="description" name="description" value="{{ old('description') : $group['description'] }}">
+    @error('description')
+    <div class="alret alert-danger">{{ $message }}</div>
+    @enderror
+</div>
+
+<button type="submit" class="btn btn-info">Submit</button>
+</form>
---
+++ resources/views/Groups/show.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('title', 'Groups')
+
+@section('content')
+<div class="card">
+    <div class="card-body">
+        <h3>Nama Teman : {{$friend['Nama']}}</h3>
+        <h3>No Telepon Teman : {{$friend['No_HP']}}</h3>
+        <h3>Alamat Teman : {{$friend['alamat']}}</h3>
+</div>
+</div>
+@endsection
